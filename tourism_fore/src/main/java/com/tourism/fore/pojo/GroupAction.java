76b1eefedbadf73a1f2e6da4eebc0f5952658e30
13527614919 <869273269@qq.com>
2019-02-27 14:39:28 +0800

    添加团游活动的实体

---
+++ tourism_fore/src/main/java/com/tourism/fore/pojo/GroupAction.java
@@ -0,0 +1,122 @@
+package com.tourism.fore.pojo;
+
+public class GroupAction {
+
+    private String gcId;              //产品编号
+    private String gcSign;            //产品类型标签
+    private String gcBriefIntroSign;  //产品简介标签
+    private String gcBriefIntro;      //产品简介内容
+    private int gcOriginPrice;     //产品原始价格
+    private int gcActivePrice;     //产品活动价格
+    private String gcFormCity;        //出发地
+    private String gcToCity;          //目的地
+    private String gctime;            //出行总时长
+    private String gcNotice;          //产品购买须知
+    private String gcRecommendDescrip;   //产品推荐理由
+    private String gcProviderId;      //产品供应商ID
+    private String gcPopularity;      //产品受欢迎人气值
+
+    public String getGcId() {
+        return gcId;
+    }
+
+    public void setGcId(String gcId) {
+        this.gcId = gcId;
+    }
+
+    public String getGcSign() {
+        return gcSign;
+    }
+
+    public void setGcSign(String gcSign) {
+        this.gcSign = gcSign;
+    }
+
+    public String getGcBriefIntroSign() {
+        return gcBriefIntroSign;
+    }
+
+    public void setGcBriefIntroSign(String gcBriefIntroSign) {
+        this.gcBriefIntroSign = gcBriefIntroSign;
+    }
+
+    public String getGcBriefIntro() {
+        return gcBriefIntro;
+    }
+
+    public void setGcBriefIntro(String gcBriefIntro) {
+        this.gcBriefIntro = gcBriefIntro;
+    }
+
+    public int getGcOriginPrice() {
+        return gcOriginPrice;
+    }
+
+    public void setGcOriginPrice(int gcOriginPrice) {
+        this.gcOriginPrice = gcOriginPrice;
+    }
+
+    public int getGcActivePrice() {
+        return gcActivePrice;
+    }
+
+    public void setGcActivePrice(int gcActivePrice) {
+        this.gcActivePrice = gcActivePrice;
+    }
+
+    public String getGcFormCity() {
+        return gcFormCity;
+    }
+
+    public void setGcFormCity(String gcFormCity) {
+        this.gcFormCity = gcFormCity;
+    }
+
+    public String getGcToCity() {
+        return gcToCity;
+    }
+
+    public void setGcToCity(String gcToCity) {
+        this.gcToCity = gcToCity;
+    }
+
+    public String getGctime() {
+        return gctime;
+    }
+
+    public void setGctime(String gctime) {
+        this.gctime = gctime;
+    }
+
+    public String getGcNotice() {
+        return gcNotice;
+    }
+
+    public void setGcNotice(String gcNotice) {
+        this.gcNotice = gcNotice;
+    }
+
+    public String getGcRecommendDescrip() {
+        return gcRecommendDescrip;
+    }
+
+    public void setGcRecommendDescrip(String gcRecommendDescrip) {
+        this.gcRecommendDescrip = gcRecommendDescrip;
+    }
+
+    public String getGcProviderId() {
+        return gcProviderId;
+    }
+
+    public void setGcProviderId(String gcProviderId) {
+        this.gcProviderId = gcProviderId;
+    }
+
+    public String getGcPopularity() {
+        return gcPopularity;
+    }
+
+    public void setGcPopularity(String gcPopularity) {
+        this.gcPopularity = gcPopularity;
+    }
+}
